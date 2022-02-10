fcdfda618e2bc6c34e8d910334a89aaa3beb6c51
Abbas Anandwala <abbasa5251@hotmail.com>
2022-02-11 01:29:27 +0530

    added models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
+from .utils import get_upload_path
 
-# Create your models here.
+class Folder(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class File(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=get_upload_path)
+    created_at = models.DateTimeField(auto_now_add=True)
