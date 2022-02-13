8a861f101fd96ad7c0ad168559360bfae5e098d6
Abbas Anandwala <abbasa5251@hotmail.com>
2022-02-13 12:33:27 +0530

    added download functionality

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HandleFileUpload, index
+from .views import HandleFileUpload, index, download
 
 urlpatterns = [
-    path('', index),
-    path('handle/', HandleFileUpload.as_view())
+    path('', index, name="index"),
+    path('download/<uuid:uid>/', download, name="download"),
+    path('handle/', HandleFileUpload.as_view(), name="file-upload-handler")
 ]
--- app/views.py
+++ app/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import FileListSerializer
-
+from rest_framework.parsers import MultiPartParser
+from rest_framework import status
 
 def index(request):
     return render(request, 'index.html')
 
+def download(request, uid):
+    context = {
+        "folder_name": uid
+    }
+    return render(request, "download.html", context=context)
+
 class HandleFileUpload(APIView):
+    parser_classes = [MultiPartParser]
     def post(self, request):
         try:
-            data = request.data
-            serializer = FileListSerializer(data=data)
-
+            serializer = FileListSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response({
-                    'status': 200,
-                    'message': 'Files uploaded successfully'
-                })
-            return Response({
-                'status': 400,
-                'message': 'Something went wrong',
-                'data': serializer.errors
-            })
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            print(e)
+            return Response({"message": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
